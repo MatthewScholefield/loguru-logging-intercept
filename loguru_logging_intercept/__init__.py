b61ab946b14c9184cb952b4ba1aef15dcbaaa58b
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2024-12-11 14:48:39 +0100

    fix import

--- loguru_logging_intercept/__init__.py
+++ loguru_logging_intercept/__init__.py
@@ -1,6 +1,6 @@
 """Module that collects all exportable items."""
 
-from loguru_logging_intercept import setup_loguru_logging_intercept
+from loguru_logging_intercept.loguru_logging_intercept import setup_loguru_logging_intercept
 
 __all__ = [
     "setup_loguru_logging_intercept",
