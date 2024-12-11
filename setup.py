10265cd308e0d7d5d8a33a0590bf5c20e4f39cc5
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2024-12-11 14:18:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ setup(
     description='Code to integrate Loguru with Python\'s standard logging module',
     long_description=LONG_DESC,
     long_description_content_type='text/markdown',
-
+    license='MIT',
     url='https://github.com/MatthewScholefield/loguru-logging-intercept',
     author='Matthew D. Scholefield',
     author_email='matthew331199@gmail.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    keywords='loguru logging intercept',
-    py_modules=['loguru_logging_intercept'],
+    keywords=['loguru', 'logging', 'intercept'],
+    packages=find_namespace_packages(include=['loguru_logging_intercept*']),
     install_requires=[
         'loguru'
     ],
